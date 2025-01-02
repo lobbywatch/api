5dc4388b8f12dc1de774b27123d404fd36b0be76
Peter Gassner <peter@gassner.io>
2025-01-02 13:17:35 +0100

    Parse the URL in the same way as Drupal v7

--- public/index.php
+++ public/index.php
@@ -5,4 +5,8 @@ require "../src/data.php";
 
 use function App\Routes\route_table_flat_id;
 
-route_table_flat_id('branche', $_GET['id']);
+// Parse the URL in the same way as Drupal v7
+[, , $path, $version, $data_type, $call_type, $object, $response_type, $response_object, $parameter] = explode("/", parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH));
+
+route_table_flat_id($object, $parameter);
+
