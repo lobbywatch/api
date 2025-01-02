e6b8a7ac105fa6d99231bd57ae905aaac3d7d186
Peter Gassner <peter@gassner.io>
2025-01-02 14:11:29 +0100

    Set language (wip)
    This doesn't work behind the scenes yet.

--- public/index.php
+++ public/index.php
@@ -6,6 +6,8 @@ require "../src/data.php";
 use App\Constants;
 use function App\domain\ApiResponse\not_found_response;
 use function App\Lib\Http\json_response;
+use function App\Lib\Localization\get_lang;
+use function App\Lib\Localization\lobbywatch_set_lang;
 use function App\Routes\{route_table_flat_id, route_table_flat_list_search};
 
 // Parse the URL in the same way as Drupal v7
@@ -17,8 +19,7 @@ if ($version !== 'v1' || $data_type !== 'json') {
   json_response(not_found_response());
 }
 
-// TODO
-//lobbywatch_set_lang(get_lang());
+lobbywatch_set_lang(get_lang());
 
 if ($call_type === 'table' && array_key_exists($object, Constants::$workflow_tables) && $response_type === 'flat' && $response_object === 'id' && $parameter) {
   route_table_flat_id($object, $parameter);
