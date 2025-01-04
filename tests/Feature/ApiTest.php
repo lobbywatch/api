c62ab0b54e366a79e961ce00317862042a06e8bb
Peter Gassner <peter@gassner.io>
2025-01-04 11:34:34 +0100

    Add more API tests

--- tests/Feature/ApiTest.php
+++ tests/Feature/ApiTest.php
@@ -21,10 +21,13 @@ test('table', function (string $path) {
   '/interessengruppe/aggregated/id/1',
   '/interessengruppe/flat/list',
 //  '/organisation/aggregated/id/1',
+//  '/organisation/flat/list?select_fields=uid,name_de,name_fr',
 //  '/parlamentarier/aggregated/id/1',
 //  '/parlamentarier/flat/id/1',
 //  '/parlamentarier/flat/list',
+//  '/parlamentarier/flat/list?select_fields=parlament_number,vorname,nachname',
 //  '/zutrittsberechtigung/aggregated/id/1',
 //  '/zutrittsberechtigung/flat/list',
+//  '/zutrittsberechtigung/flat/list?select_fields=id,vorname,nachname',
 ]);
 
