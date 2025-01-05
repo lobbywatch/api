7e91fcaa26677f1d7a2ae5b0d1e2ca42d3175a2e
Peter Gassner <peter@gassner.io>
2025-01-05 11:05:04 +0100

    Use original user_access function definition

--- src/domain/IdentityAccess.php
+++ src/domain/IdentityAccess.php
@@ -4,6 +4,13 @@ declare(strict_types=1);
 namespace App\Domain\IdentityAccess;
 
 function user_access(string $rule) {
-  // TODO
-  return false;
+  switch ($rule) {
+    case 'access lobbywatch general content':
+      return true;
+    case 'access lobbywatch advanced content':
+    case 'access lobbywatch unpublished content':
+    case 'access lobbywatch admin':
+    default:
+      return false;
+  }
 }
