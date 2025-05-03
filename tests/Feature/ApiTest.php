45fa654e2a2c249f523d2e0c0648588ff34a80be
Peter Gassner <peter@gassner.io>
2025-05-03 16:46:29 +0200

    Add missing tests

--- tests/Feature/ApiTest.php
+++ tests/Feature/ApiTest.php
@@ -18,13 +18,23 @@ test('table', function (string $path, array $query = []) {
 })->with([
   '/branche/aggregated/id/1',
   '/branche/flat/list',
+  ['/branche/flat/list', ['select_fields' => 'id,name']],
+
   '/interessengruppe/aggregated/id/1',
   '/interessengruppe/flat/list',
+  ['/interessengruppe/flat/list', ['select_fields' => 'id,name']],
+
   '/organisation/aggregated/id/2',
-  ['/organisation/flat/list', ['select_fields' => 'uid,name_de,name_fr,rechtsform']],
+  // Querying this without any field restrictions exhausts the memory
+  [
+    '/organisation/flat/list',
+    ['select_fields' => 'name_de,name_fr,rechtsform,ort,abkuerzung_de,abkuerzung_fr,interessengruppe_de,interessengruppe_fr,interessengruppe_id,interessengruppe2_de,interessengruppe2_fr,interessengruppe2_id,interessengruppe3_de,interessengruppe3_fr,interessengruppe3_id,uid,alias_namen_de,alias_namen_fr']
+  ],
+
   '/parlamentarier/aggregated/id/6',
   '/parlamentarier/flat/list',
   ['/parlamentarier/flat/list', ['select_fields' => 'parlament_number,vorname,nachname']],
+
   '/zutrittsberechtigung/aggregated/id/1566',
   '/zutrittsberechtigung/flat/list',
   ['/zutrittsberechtigung/flat/list', ['select_fields' => 'id,vorname,nachname']],
