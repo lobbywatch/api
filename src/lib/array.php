f43679526363c36e0787411814b59bbd92384b70
Peter Gassner <peter@gassner.io>
2025-06-30 20:26:01 +0200

    Fix Zutrittsberechtigungen
    The reason was that max_attribute_in_array couldn't deal with empty arrays and crashed, leading to the rest of the data not being returned.

--- src/lib/array.php
+++ src/lib/array.php
@@ -5,5 +5,5 @@ namespace App\Lib\Array;
 
 // https://stackoverflow.com/questions/36602362/how-can-i-find-the-max-attribute-in-an-array-of-objects
 function max_attribute_in_array($array, $prop) {
-  return max(array_column($array, $prop));
+  return empty($array) ? 0 : max(array_column($array, $prop));
 }
